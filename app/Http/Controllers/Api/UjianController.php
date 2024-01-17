6aa4e9702b292f451b017388dd582854d53b3849
Mahpudin <pudinmah69@gmail.com>
2024-01-17 10:37:06 +0700

    add status ujian, timer

--- app/Http/Controllers/Api/UjianController.php
+++ app/Http/Controllers/Api/UjianController.php
@@ -106,18 +106,28 @@ class UjianController extends Controller
             ], 200); //404
         }
         $ujianSoalList = UjianSoalList::where('ujian_id', $ujian->id)->get();
+        $soalIds = $ujianSoalList->pluck('soal_id');
 
+        // dd($soalIds);
 
         $ujianSoalListId = [];
         foreach ($ujianSoalList as $soal) {
             array_push($ujianSoalListId, $soal->soal_id);
         }
 
-        $soal = Soal::whereIn('id', $ujianSoalListId)->where('kategori', $request->kategori)->get();
 
+        $soal = Soal::whereIn('id', $ujianSoalListId)->where('kategori', $request->kategori)->get();
+        //timer by kategori
+        $timer = $ujian->timer_angka;
+        if ($request->kategori == 'Verbal') {
+            $timer = $ujian->timer_verbal;
+        } else if ($request->kategori == 'Logika') {
+            $timer = $ujian->timer_logika;
+        }
 
         return response()->json([
             'message' => 'Berhasil mendapatkan soal',
+            'timer' => $timer,
             'data' => SoalResource::collection($soal),
         ]);
     }
